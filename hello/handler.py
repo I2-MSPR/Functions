ef7b99cc574481680a6738eb0731ef72bd795daf
DenisJongmanee <jongmanee.denis@gmail.com>
2025-06-26 14:30:33 +0200

    resolve cors policy

--- hello/handler.py
+++ hello/handler.py
@@ -6,5 +6,9 @@ def handle(event, context):
             "id": 1,
             "username": username,
             "password": "RXBzaS4xMjM=",
+        },
+        "headers": {
+            "Content-type": "text/plain",
+            "Access-Control-Allow-Origin": "http://127.0.0.1:8000"
         }
     }
