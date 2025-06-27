19255d8064e664c0519efa66e8e18101607f491b
DenisJongmanee <jongmanee.denis@gmail.com>
2025-06-27 12:27:16 +0200

    update hello to test front

--- hello/handler.py
+++ hello/handler.py
@@ -1,14 +1,17 @@
 def handle(event, context):
+    usernames = ["Ikhlas", "Rémi", "Gaëtan", "Denis"]
     username = event.query['username']
+    password = event.query['password']
+    success = username in usernames and password == "RXBzaS4xMjM="
+    
     return {
-        "statusCode": 200,
+        "statusCode": 200 if success else 401,
         "body": {
-            "id": 1,
-            "username": username,
-            "password": "RXBzaS4xMjM=",
+            "message": "correct IDs" if success else "Not found"
         },
         "headers": {
             "Content-type": "text/plain",
             "Access-Control-Allow-Origin": "http://127.0.0.1:8000"
         }
     }
+
