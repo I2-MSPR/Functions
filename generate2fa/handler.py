3726d5a90ffc7f5169675fb61f4705645a4cacb5
DenisJongmanee <jongmanee.denis@gmail.com>
2025-07-07 11:02:33 +0200

    fix:  generate2fa

--- generate2fa/handler.py
+++ generate2fa/handler.py
@@ -3,46 +3,68 @@ import qrcode
 import qrcode.image.svg
 import base64
 import json
-from google.cloud.sql.connector import Connector, IPTypes
-import pymysql 
+import pymysql
+import mysql.connector
+from io import BytesIO
 
 def handle(event, context):
     try :
         key = generate_secret()
         qr = generate_qrcode(key, event.query["username"])
         encoded_key = crypt_secret(key)
-        time_test=database_storage(encoded_key, event.query["username"])
+        userExists = database_storage(encoded_key, event.query["username"])
+        if not userExists:
+            return {
+                "statusCode": 404,
+                "body": "User not Found",
+            "headers": {
+                "Content-type": "text/plain",
+                "Access-Control-Allow-Origin": "http://127.0.0.1:8000"
+            }
+            }
         return {
             "statusCode": 200,
-            "body": {"qrcode": qr.to_string().decode("utf-8"), "time":time_test}
+            "body": { "qrcode" : qr },
+            "headers": {
+                "Content-type": "text/plain",
+                "Access-Control-Allow-Origin": "http://127.0.0.1:8000"
+            }
         }
-        """
-        if database_storage(encoded_key, event.query["username"]) :
-            return {
-                "statusCode": 200,
-                "body": {"qrcode": qr.to_string().decode("utf-8")}
-            }
-        else :
-            return {
-                "statusCode": 500
-            }
-            """
     except:
-        raise 
-    """
         return {
-            "statusCode": 500
+            "statusCode": 500,
+            "body": { "qr": qr, "userExists" : userExists },
+            "headers": {
+                "Content-type": "text/plain",
+                "Access-Control-Allow-Origin": "http://127.0.0.1:8000"
+            }
         }
-        """
-
 
 def generate_secret():
     return pyotp.random_base32()
 
 def generate_qrcode(key, username):
     uri = pyotp.totp.TOTP(key).provisioning_uri(name=username, issuer_name="Cloud Connect")
-    factory = qrcode.image.svg.SvgPathImage
-    return qrcode.make(uri, image_factory=factory)
+    
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(uri)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    buffer = BytesIO()
+    img.save(buffer, format="PNG")
+    buffer.seek(0)
+
+    img_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+    
+    return img_base64
+
 
 def crypt_secret(key):
     encoded_binary = (base64.b64encode(key.encode('ascii')))
@@ -50,23 +72,25 @@ def crypt_secret(key):
 
 def database_storage(encoded_key, username):
     connection = getconn()
-    with connection.cursor() as cur:
-        cur.execute("SELECT NOW()")
-        data = cur.fetchone()[0]
-    connection.close()
-    return data
+    with connection.cursor(dictionary=True) as cur:
+        cur.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        if user is None:
+            return False
+        else:
+            id = user["id"]
+            cur.execute("UPDATE users SET mfa = %s WHERE id = %s", (encoded_key, id))
+            connection.commit()
+            return True
 
 def getconn() -> pymysql.connections.Connection:
-    mdp = get_secret("database-mdp")
-    with Connector() as connector:
-        return connector.connect(
-            "firm-reason-462012-p9:europe-west1:mspr2",
-            "pymysql",
-            user="cloud-connect-prod",
-            password=mdp,
-            db="cloud_connect",
-            ip_type=IPTypes.PUBLIC,   
-        )
+    mdp = get_secret("password")
+    return mysql.connector.connect(
+        host="mysql.openfaas-fn.svc.cluster.local",
+        user="root",
+        password=mdp,
+        database="cloud-connect"
+    )
 
 def get_secret(key):
     with open("/var/openfaas/secrets/{}".format(key)) as f:
